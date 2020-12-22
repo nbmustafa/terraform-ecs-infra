5e9d7b03c1011076fac1ab3b2e1b0790f0b12af9
p792405 <nashwan.mustafa@nab.com.au>
2020-12-22 12:17:06 +1100

    updated ami

--- vars.tf
+++ vars.tf
@@ -5,12 +5,21 @@ variable "service_name" {
 }
 
 variable "app_name" {
+  default     = "abc"
   type        = string
   description = "The app name that all resources will be tagged with"
 }
 
 variable "environment" {
-  default     = "dev"
+  default     = "develop"
   type        = string
   description = "The development stage this deployment is for, e.g. dev01, sit02, prod01"
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable environment_name {
+  type    = string
+  default = "develop"
 }
