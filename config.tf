dc4d83037ba33a57c19ef3a326934ecb521a6bd3
p792405 <nashwan.mustafa@nab.com.au>
2020-12-22 11:58:30 +1100

    updated ami

--- config.tf
+++ config.tf
@@ -1,11 +1,24 @@
+# provider "aws" {
+#   region  = "ap-southeast-2"
+#   version = "~> 2.22"
+# }
+
+# terraform {
+#   backend "s3" {
+#     region = "ap-southeast-2"
+#     key    = "state"
+#   }
+# }
+
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+
 provider "aws" {
-  region  = "ap-southeast-2"
-  version = "~> 2.22"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  profile    = "default"
+  region     = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    region = "ap-southeast-2"
-    key    = "state"
-  }
-}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+aws_access_key = "AKIAVOKCFVROSBRRY7F2"
+aws_secret_key = "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"
