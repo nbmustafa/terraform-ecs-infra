f6a13d51e72eeec3c31c4589dec3f5b163e8db57
nashwan <nashwan.bilal@gmail.com>
2020-12-24 18:24:05 +1100

    updated data source

--- modules/ecs-cluster/data.tf
+++ modules/ecs-cluster/data.tf
@@ -4,24 +4,27 @@ data aws_region current {
 data aws_caller_identity current {
 }
 
-
-data "aws_vpc" "vpc" {
+data aws_vpc vpc {
   filter {
     name   = "tag:Name"
     values = ["*default*"]
   }
 }
 
+data aws_subnet_ids subnet_ids {
+  vpc_id = data.aws_vpc.vpc.id
+}
+
+data aws_security_group vpc_default_sg {
+  vpc_id = data.aws_vpc.vpc.id
+  name   = "default"
+}
+
 # data "aws_ssm_parameter" "ecs_ami" {
 #   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 # }
-
 # image_id      = data.aws_ssm_parameter.ecs_ami.value
 
-
-# data "aws_ssm_parameter" "ecs_ami_amzn_lnx" {
-#   name = "/aws/service/ecs/optimized-ami/amazon-linux/recommended/image_id"
-# }
 
 data "aws_ami" "ecs_ami" {
   most_recent = true
@@ -51,14 +54,4 @@ data "template_file" "user_data" {
   vars = {
     ecs_cluster_name = aws_ecs_cluster.ecs_cluster.name
   }
-}
-
-
-data "aws_subnet_ids" "subnet_ids" {
-  vpc_id = data.aws_vpc.vpc.id
-}
-
-data "aws_security_group" "vpc_default_sg" {
-  vpc_id = data.aws_vpc.vpc.id
-  name   = "default"
 }
