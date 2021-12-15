a8a096b49742227b33e6bcdbd9fc6173f2c2633a
Nashwan Bilal <nashwan.bilal@gmail.com>
2021-12-15 12:34:34 +1100

    Update locals.tf

--- modules/ecs-cluster/locals.tf
+++ modules/ecs-cluster/locals.tf
@@ -1,5 +1,34 @@
 locals {
   region = data.aws_region.current.name
   prefix = "${var.service_name}-${var.app_name}-${var.environment}"
+  
+  asg_tags = [ 
+    { 
+      key                 = "AppName"
+      value               = var.app_name
+      propagate_at_launch = true
+    },
+    { 
+      key                 = "Environment"
+      value               = var.environment
+      propagate_at_launch = true
+    },
+    { 
+      key                 = "ServiceName"
+      value               = var.service_name
+      propagate_at_launch = true
+    },
+    { 
+      key                 = "CostCentre"
+      value               = var.cost_centre
+      propagate_at_launch = true
+    },
+    { 
+      key                 = "PowerMgmt"
+      value               = var.PowerMgmt
+      propagate_at_launch = true
+    },
+  ]
+  
 }
 
